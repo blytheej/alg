940b9d7ab56460787483d17832a78873bebf7e79
Teemo <blytheej@gmail.com>
2018-08-28 21:28:33 +0900

    재업로드

--- bfs/2589-보물섬.cpp
+++ bfs/2589-보물섬.cpp
@@ -3,7 +3,7 @@
 
 int n = 0, m = 0;
 char map[51][51];
-char far[51][51]; //¸Ê visit
+char far[51][51]; //ë§µ visit
 int furthest = 0;// furthest route
 int howfar = 0; //how far from start
 std::queue<int> f_x;
@@ -38,7 +38,7 @@ int get_far() {
 		f_x.push(x - 1);
 		f_y.push(y);
 	}
-	if (x + 1 < n && map[x + 1][y] == 'L' && far[x + 1][y] == 0) {
+	if (x +1 < n && map[x + 1][y] == 'L' && far[x + 1][y] == 0) {
 		far[x + 1][y] = far[x][y] + 1;
 		f_x.push(x + 1);
 		f_y.push(y);
@@ -48,7 +48,7 @@ int get_far() {
 		f_x.push(x);
 		f_y.push(y - 1);
 	}
-	if (y + 1 < m && map[x][y + 1] == 'L' && far[x][y + 1] == 0) {
+	if(y + 1 < m && map[x][y + 1] == 'L' && far[x][y + 1] == 0) {
 		far[x][y + 1] = far[x][y] + 1;
 		f_x.push(x);
 		f_y.push(y + 1);
@@ -62,12 +62,10 @@ int main() {
 	//input
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			scanf("%c", &map[i][j]);
+			scanf(" %c", &map[i][j]);
 		}
-		scanf("%c", &enter);
 	}
-
-
+	
 	//get_furthest
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
@@ -85,8 +83,8 @@ int main() {
 			}
 		}
 	}
-
-
+	
+	
 	printf("%d", furthest);
 
 	return 0;
